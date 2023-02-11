32e5fa045db885e1ed435ff15d57955743c23e58
JeanPierre Janopoulos <jean-pierre.janopoulos@epitech.com>
2023-02-11 16:49:00 +0100

    [FIX] bullets now rotate properly with the right angle

--- JAM.py
+++ JAM.py
@@ -8,7 +8,7 @@ pygame.init()
 # Create the screen
 height = 800
 width = 600
-screen = pygame.display.set_mode((height, width))
+screen = pygame.display.set_mode((width, height))
 font = pygame.font.SysFont(None, 30)
 score_text = 0
 
@@ -21,7 +21,7 @@ GREY = (128, 128, 128)
 # Define speed of game elements
 player_speed = 5
 speed_shoot = 10
-speed_enemy = 10
+speed_enemy = 0
 
 # Define mouse usage
 mouse_pos = [0, 0]
@@ -54,17 +54,24 @@ class Player:
 
 
 # Define shoot
-
+def get_rotate_angle(vect):
+    rotation = 0
+    if (vect[1] > 0):
+        rotation += 180
+    rotation -= (vect[0] / math.sqrt(math.pow(vect[0], 2) + math.pow(vect[1], 2))) * 90
+    return rotation
 
 class Shoot:
     def __init__(self, x, y, dest):
-        self.img = shoot_img
+        self.vect = [dest[0] - x, dest[1] - y]
+        self.norm_vect = [math.sqrt(math.pow(self.vect[0], 2) + math.pow(self.vect[1], 2)), math.sqrt(math.pow(self.vect[0], 2) + math.pow(self.vect[1], 2))]
+        self.speed_x = self.vect[0] / self.norm_vect[0] * speed_shoot
+        self.speed_y = self.vect[1] / self.norm_vect[1] * speed_shoot
+        self.rotation = get_rotate_angle(self.vect)
+        self.img = pygame.transform.rotate(shoot_img, self.rotation)
         self.rect = self.img.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed_x = (dest[0] - x) / math.sqrt(math.pow(dest[0] - x, 2) + math.pow(dest[1] - y, 2)) * speed_shoot
-        self.speed_y = (dest[1] - y) / math.sqrt(math.pow(dest[0] - x, 2) + math.pow(dest[1] - y, 2)) * speed_shoot
-        self.dest = dest
 
     def move(self):
         self.rect.x += self.speed_x
@@ -74,8 +81,6 @@ class Shoot:
         screen.blit(self.img, self.rect)
 
 # Define enemy
-
-
 class Enemy:
     def __init__(self, x, y):
         self.img = enemy_img
@@ -132,7 +137,7 @@ while running:
         player.move_right()
     if mouse_button[0]:
         now = pygame.time.get_ticks()
-        if now - last_shoot > 500:
+        if now - last_shoot > 50:
             last_shoot = now
             shoots.append(player.shoot(mouse_pos))
 
@@ -176,11 +181,11 @@ while running:
 
     if win:
         win_text = font.render('You win!', True, BLACK)
-        screen.blit(win_text, (width / 1.5 - win_text.get_width() /
+        screen.blit(win_text, (width / 2.5 - win_text.get_width() /
                     2, height / 3 - win_text.get_height() / 2))
     elif lose:
         lose_text = font.render('You lose!', True, BLACK)
-        screen.blit(lose_text, (width / 1.5 - lose_text.get_width() /
+        screen.blit(lose_text, (width / 2.5 - lose_text.get_width() /
                     2, height / 3 - lose_text.get_height() / 2))
 
     # Update display
